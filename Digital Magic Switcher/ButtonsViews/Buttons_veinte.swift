fab4b76214f465248947d8322d0734a99d02f3d8
omar <ocampos97@outlook.com>
2020-12-17 12:33:53 -0600

    change button function disable

--- Digital Magic Switcher/ButtonsViews/Buttons_veinte.swift
+++ Digital Magic Switcher/ButtonsViews/Buttons_veinte.swift
@@ -37,38 +37,39 @@ struct Buttons_veinte: View {
     @Binding var indexAux : Int
     @Binding var index : Int
     @Binding var showB1 : [Bool]
+    @Binding var showB2 : [Bool]
     var body: some View {
        
         HStack{
-            boton(nombre:self.$channelsName,id: self.$setid1, ip: self.$ip ,num: nums[0], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid2, ip: self.$ip ,num: nums[1], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid3, ip: self.$ip ,num: nums[2], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid4, ip: self.$ip ,num: nums[3], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid5, ip: self.$ip ,num: nums[4], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
+            boton(nombre:self.$channelsName,id: self.$setid1, ip: self.$ip ,num: nums[0], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid2, ip: self.$ip ,num: nums[1], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid3, ip: self.$ip ,num: nums[2], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid4, ip: self.$ip ,num: nums[3], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid5, ip: self.$ip ,num: nums[4], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
         }
         
         HStack{
-            boton(nombre:self.$channelsName,id: self.$setid6, ip: self.$ip ,num: nums[5], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid7, ip: self.$ip ,num: nums[6], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid8, ip: self.$ip ,num: nums[7], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid9, ip: self.$ip ,num: nums[8], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid10, ip: self.$ip ,num:nums[9], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
+            boton(nombre:self.$channelsName,id: self.$setid6, ip: self.$ip ,num: nums[5], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid7, ip: self.$ip ,num: nums[6], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid8, ip: self.$ip ,num: nums[7], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid9, ip: self.$ip ,num: nums[8], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid10, ip: self.$ip ,num:nums[9], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
         }
         
         HStack{
-            boton(nombre:self.$channelsName,id: self.$setid11, ip: self.$ip ,num: nums[10], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid12, ip: self.$ip ,num: nums[11], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid13, ip: self.$ip ,num: nums[12], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid14, ip: self.$ip ,num: nums[13], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid15, ip: self.$ip ,num: nums[14], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
+            boton(nombre:self.$channelsName,id: self.$setid11, ip: self.$ip ,num: nums[10], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid12, ip: self.$ip ,num: nums[11], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid13, ip: self.$ip ,num: nums[12], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid14, ip: self.$ip ,num: nums[13], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid15, ip: self.$ip ,num: nums[14], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
         }
         
         HStack{
-            boton(nombre:self.$channelsName,id: self.$setid16, ip: self.$ip ,num: nums[15], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid17, ip: self.$ip ,num: nums[16], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid18, ip: self.$ip ,num: nums[17], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid19, ip: self.$ip ,num: nums[18], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
-            boton(nombre:self.$channelsName,id: self.$setid20, ip: self.$ip ,num: nums[19], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index)
+            boton(nombre:self.$channelsName,id: self.$setid16, ip: self.$ip ,num: nums[15], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid17, ip: self.$ip ,num: nums[16], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid18, ip: self.$ip ,num: nums[17], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid19, ip: self.$ip ,num: nums[18], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
+            boton(nombre:self.$channelsName,id: self.$setid20, ip: self.$ip ,num: nums[19], buttons: self.$showB1, collection: self.$collection, activo: self.$activo1, tamaño: .infinity, aux: self.$indexAux, index: self.$index, disable: self.$showB2)
         }
         
         
